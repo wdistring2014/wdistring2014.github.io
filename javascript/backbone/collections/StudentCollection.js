9e557e092f3984d7afc7be3cca456ac8d232d818
daramao <dara.mao@gmail.com>
2014-08-21 15:15:21 -0400

    Add template

--- javascript/backbone/collections/StudentCollection.js
+++
@@ -1,3 +0,0 @@
-var StudentCollection = Backbone.Collection.extend({
-  model: Student
-})
--- javascript/backbone/views/StudentView.js
+++ javascript/backbone/views/StudentView.js
@@ -1,16 +1,16 @@
 var StudentView = Backbone.View.extend({
-  tagName: "li",
+    tagName: "li",
 
-  template: _.template("<%= name %>"),
+    template: _.template( $("#studentTemplate").html() ),
 
-  initialize: function() {
-    this.render();
-  },
+    initialize: function() {
+        this.render();
+    },
 
-  render: function() {
-    this.$el.html( this.template(this.model.toJSON()) );
-  }
-
+    render: function() {
+        this.$el.html( this.template(this.model.toJSON()) );
+        return this;
+    }
 });
 
 var student = new Student;
